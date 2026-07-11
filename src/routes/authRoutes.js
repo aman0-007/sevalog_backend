881e71feb843834661135c4d2b5b77d627966b5f
aman0-007 <dwivedi.aman2811@gmail.com>
2026-07-11 13:37:50 +0530

    first commit

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const { verifyToken } = require('../middlewares/authMiddleware');
 
 // Route for volunteer registration
 router.post('/register', authController.register);
